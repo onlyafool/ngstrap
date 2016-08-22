fe9697038387144e61673e7d4d275ff18f4a7760
onlyafool <onlyafool@gmx.net>
2016-08-22 11:49:53 +0200

    first commit

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package ngstrap
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'ngstrap' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'ngstrap' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'ngstrap' ), 'ngstrap', '<a href="http://www.kohlmann-medienkontor.de" rel="designer">KMK</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
