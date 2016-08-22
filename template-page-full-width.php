330d024abfafe0972f5f1f761de1e5cdb1eb5b70
onlyafool <onlyafool@gmx.net>
2016-08-22 17:08:00 +0200

    Full-Width-Template added

---
+++ template-page-full-width.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Template Name: Full Width Page
+ *
+ * The template for displaying full width pages.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package ngstrap
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area content-area-full">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+
+get_footer();
